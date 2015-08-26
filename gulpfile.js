075adee3b2b1802c3bd3f188cbe4717a21f2ba84
Yosuke <yosuketto@gmail.com>
2015-08-26 16:08:13 +0900

    use babelify

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,29 @@ var gulp = require('gulp'),
     jade = require('gulp-jade'),
     babel = require('gulp-babel'),
     del = require('del');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var through2 = require('through2');
 
 var vendor_components_dir = "./app/vendor_components";
 var styles_dir = "./app/styles";
 var scripts_dir = "./app/scripts";
 var regJsfile = /\.js/;
+
+var babelify_compiler = function () {
+  function compile(file){
+    return browserify(file.path, file.base).transform(babelify)
+      .bundle().pipe(source(file.relative));
+  }
+  return through2({objectMode: true}, function(chunk, enc, callback){
+    var up = through2({objectMode: true}, function(compiled, e, cb){
+      callback(null, compiled);
+      cb();
+    });
+    compile(chunk).pipe(up);
+  });
+}
 
 gulp.task("clean:styles", function(cb){
   del([styles_dir], cb);
@@ -43,5 +61,7 @@ gulp.task("clean:scripts", function(cb){
   del([scripts_dir], cb);
 });
 gulp.task("scripts", ["clean:scripts"], function(){
-  return gulp.src("src/*.js").pipe(babel()).pipe(gulp.dest(scripts_dir));
+  return gulp.src("src/**/*.js").pipe(babelify_compiler()).pipe(gulp.dest(scripts_dir));
 });
+
+gulp.task("build", ["vendor-files", "styles", "html", "scripts"])
